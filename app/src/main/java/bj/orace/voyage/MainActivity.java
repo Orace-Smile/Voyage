0d8d9960fededdef4a84e1e93b12fa9d2e1e11b7
Enjoytec <oracejuscard@gmail.com>
2022-10-14 17:22:42 +0100

    premier mise a jour

--- app/src/main/java/bj/orace/voyage/MainActivity.java
+++ app/src/main/java/bj/orace/voyage/MainActivity.java
@@ -2,13 +2,38 @@ package bj.orace.voyage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+
+import bj.orace.voyage.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView app_image;
+    private CircularProgressIndicator loader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        app_image = (ImageView) findViewById(R.id.app_image);
+        loader = (CircularProgressIndicator) findViewById(R.id.loader);
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        new Handler().postDelayed(runnable,5000);
+
     }
 }
